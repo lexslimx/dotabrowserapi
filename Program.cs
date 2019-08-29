5892021f2a24ae138df0787051c61f660b7512fd
Wamwea <Mynameiswamwea@gmail.com>
2019-08-29 14:22:36 +0300

    what do i do?

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace dotabrowserapi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("H");
         }
     }
 }
