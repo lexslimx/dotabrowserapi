dee08a872a770cb7efbe36461accbce71eda5891
Wamwea <Mynameiswamwea@gmail.com>
2019-08-30 17:44:16 +0300

    created the API shell/skeleton

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace dotabrowserapi
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            console.Writeline("hello");
+            CreateWebHostBuilder(args).Build().Run();
         }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
